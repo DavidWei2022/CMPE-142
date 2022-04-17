f55b5fc80ff1aa82bdf6db61df7987c4cafe2b78
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2022-04-16 17:01:08 -0700

    Initial commit

---
+++ src/CMPE 142.c
@@ -0,0 +1,69 @@
+/*
+ ============================================================================
+ Name        : CMPE.c
+ Author      : David Wie
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include <pthread.h>
+
+//pthread_t tid[2];
+//int counter;
+
+//void* multiThreads(void *arg)
+//{
+//    unsigned long i = 0;
+//    counter += 1;
+//    printf("\n Job %d started\n", counter);
+//
+//    for(i=0; i<(0xFFFFFFFF);i++);
+//    printf("\n Job %d finished\n", counter);
+//
+//    return NULL;
+//}
+
+int main() {
+//	printf("hi");
+	struct linked_list *head = ll_create();
+//	struct linked_list *head2= pthread_create(head,NULL,&multiThreads,NULL);
+	printf("length is %d\n", ll_length(head));
+	ll_add(head, 1);
+	printf("length is %d\n", ll_length(head));
+	ll_add(head, 2);
+	ll_add(head, 3);
+	ll_add(head, 5);
+	ll_remove_first(head);
+	ll_remove_first(head);
+	printf("length is %d\n", ll_length(head));
+	int test_var = 5;
+	if (ll_contains(head, test_var)) {
+		printf("It contains %d\n", test_var);
+	} else {
+		printf("It does not contains %d\n", test_var);
+	}
+	test_var = 1;
+	if (ll_contains(head, test_var)) {
+		printf("It contains %d\n", test_var);
+	} else {
+		printf("It does not contains %d\n", test_var);
+	}
+	ll_remove_first(head);
+	printf("length is %d\n", ll_length(head));
+	if (ll_destroy(head) == 0) {
+		printf("could not destroy the linked list!\n");
+	}
+	ll_remove_first(head);
+	//ll_destroy should not perform its function since the linked list is not empty
+	if (ll_destroy(head) == 1) {
+		printf("successfuly destroy the linked list!\n");
+	}
+
+	return 0;
+}
+
---
+++ src/list.h
@@ -0,0 +1,159 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+struct linked_list {
+	int val;
+	struct linked_list *next;
+	pthread_mutex_t *mutex;
+
+};
+
+static inline struct linked_list*
+ll_create(void) {
+
+	//consider the case when the memory is running out
+	struct linked_list *node = malloc(sizeof(struct linked_list));
+
+	if (pthread_mutex_init(node->mutex, NULL)) {
+		printf("not init\n");
+	} else {
+		printf("successfully init\n");
+	}
+
+	if (!node) {
+		return NULL;
+	} else {
+		node->val = NULL;
+		node->next = NULL;
+		return node;
+	}
+}
+
+static inline int ll_length(struct linked_list *ll) {
+//	pthread_mutex_lock(ll->mutex);
+	if (ll->val == NULL) {
+//		pthread_mutex_unlock(ll->mutex);
+		return 0;
+	} else {
+		int len = 0;
+		struct linked_list *node;
+		node = ll;
+		while (node->val != NULL) {
+			//		printf("%d\n",node->val);
+			node = node->next;
+			//		ll = temp;
+			len++;
+		}
+//		pthread_mutex_unlock(ll->mutex);
+		return len;
+	}
+}
+
+static inline int ll_destroy(struct linked_list *ll) {
+	if (pthread_mutex_lock(ll->mutex) == 0) {
+		printf("Lock successful\n");
+	} else {
+		printf("Lock Unsuccessful\n");
+	}
+
+	//check if the linked list is empty, if it is not, don't deallocate it
+	if (ll_length(ll) != 0) {
+		pthread_mutex_unlock(ll->mutex);
+		return 0;
+	} else {
+		pthread_mutex_unlock(ll->mutex);
+		free(ll);
+		ll = NULL;
+
+		//return 1 if it is destroyed, 0 if it is not destroyed
+		if (ll == NULL) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+static inline void ll_add(struct linked_list *ll, int value) {
+	if (pthread_mutex_lock(ll->mutex) == 0) {
+		printf("Lock successful\n");
+	} else {
+		printf("Lock Unsuccessful\n");
+	}
+//	struct linked_list *temp = ll->next;
+	struct linked_list *new = malloc(sizeof(struct linked_list));
+//make new into ll, then change the value of ll into the parameter
+	new->val = ll->val;
+	new->next = ll->next;
+	ll->val = value;
+	ll->next = new;
+	pthread_mutex_unlock(ll->mutex);
+}
+
+static inline bool ll_remove_first(struct linked_list *ll) {
+	if (pthread_mutex_lock(ll->mutex) == 0) {
+		printf("Lock successful\n");
+	} else {
+		printf("Lock Unsuccessful\n");
+	}
+//	printf("%d",ll->val);
+//	struct linked_list* temp;
+	if (ll->val == NULL) {
+		//		printf("no node!\n");
+		return false;
+	}
+//if the linked list only has one node, remove the node
+	else if (ll->next == NULL) {
+		//		printf("only one node!\n");
+		pthread_mutex_unlock(ll->mutex);
+		ll->val = NULL;
+		ll->next = NULL;
+		return 1;
+	} else {
+		pthread_mutex_unlock(ll->mutex);
+		//		printf("so many nodes!\n");
+		struct linked_list *temp = ll->next;
+		//		temp->val = ll->val;
+		//		new->next = ll->next;
+		ll->val = ll->next->val;
+		ll->next = ll->next->next;
+		free(temp);
+
+		return 1;
+	}
+}
+
+static inline int ll_contains(struct linked_list *ll, int value) {
+	if (pthread_mutex_lock(ll->mutex) == 0) {
+		printf("Lock successful\n");
+	} else {
+		printf("Lock Unsuccessful\n");
+	}
+	if (ll == NULL) {
+		return 0;
+	} else {
+		pthread_mutex_unlock(ll->mutex);
+		struct linked_list *temp;
+		struct linked_list *head;
+		int position = 1;
+		while (ll->next != NULL) {
+			if (value != ll->val) {
+				temp = ll->next;
+				ll = temp;
+
+				position++;
+			} else {
+				return position;
+			}
+		}
+	}
+
+	return 0;
+}
+
+#endif
